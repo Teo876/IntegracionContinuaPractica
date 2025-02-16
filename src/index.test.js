b5722ce193b9067fe4f4bd2642b29c6eb20d9133
LauraCantalicio <20211845@itla.edu.do>
2025-02-16 19:14:32 -0400

    Add GitHub Actions CI workflow

---
+++ src/index.test.js
@@ -0,0 +1,7 @@
+// index.test.js
+const { execSync } = require('child_process');
+
+test('prints Hola Mundo', () => {
+  const output = execSync('node index.js').toString();
+  expect(output).toBe('Hola Mundo\n');
+});

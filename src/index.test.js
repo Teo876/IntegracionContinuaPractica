a824b9985dff7121a507ae472eacedf869fbde37
Teo876 <20211845@itla.edu.do>
2025-02-16 22:29:17 -0400

    Mover archivos JS a la raíz del proyecto y eliminar de src

--- src/index.test.js
+++
@@ -1,7 +0,0 @@
-// index.test.js
-const { execSync } = require('child_process');
-
-test('prints Hola Mundo', () => {
-  const output = execSync('node index.js').toString();
-  expect(output).toBe('Hola Mundo\n');
-});
